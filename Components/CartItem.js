263f16a1504842110f8c4f803c889c77fec73ef8
Hydra016 <haidermansoor459@gmail.com>
2022-06-16 12:58:09 +0300

    'delete-modal'

--- Components/CartItem.js
+++ Components/CartItem.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import Modal from "./Modal"; 
+
 
 const CartItem = ({ item, deleteItem }) => {
-    console.log(item)
+    const [visible, setVisible] = useState(false);
 
     return (
         <View style={styles.container}>
+            <Modal 
+      visible={visible}
+      title='Are you sure you want to remove this item from your cart?'
+      setModalVisible={setVisible}
+      onPressYes={() => deleteItem(item.id)}
+      />
             <Image style={styles.image} source={{ uri: item.mainImg }} />
             <View style={styles.itemContainer}>
                 <View style={styles.itemDetails}>
@@ -26,7 +34,7 @@ const CartItem = ({ item, deleteItem }) => {
                 <Text>{item.chip}</Text>
                 </View>
             </View>
-            <TouchableOpacity style={styles.button} onPress={() => deleteItem(item.id)}>
+            <TouchableOpacity style={styles.button} onPress={() => setVisible(true)}>
                 <Ionicons style={styles.deleteIcon} name='trash-outline' />
             </TouchableOpacity>
         </View>
